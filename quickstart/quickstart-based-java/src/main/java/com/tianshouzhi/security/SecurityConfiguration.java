7e4cb437ef64ade04ff63bbaa813d01f6c30bca3
tianshouzhi <tianshouzhi@126.com>
2017-12-16 21:25:13 +0800

    remove unused import

--- quickstart/quickstart-based-java/src/main/java/com/tianshouzhi/security/SecurityConfiguration.java
+++ quickstart/quickstart-based-java/src/main/java/com/tianshouzhi/security/SecurityConfiguration.java
@@ -1,11 +1,8 @@
 package com.tianshouzhi.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.AbstractConfiguredSecurityBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by tianshouzhi on 2017/12/16.
