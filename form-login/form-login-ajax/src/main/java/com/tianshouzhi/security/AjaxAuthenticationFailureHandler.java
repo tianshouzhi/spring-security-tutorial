740c66cdbf8ab49898df186e8968189458802808
tianshouzhi <tianshouzhi@126.com>
2017-12-19 01:16:29 +0800

    and angular js

---
+++ form-login/form-login-ajax/src/main/java/com/tianshouzhi/security/AjaxAuthenticationFailureHandler.java
@@ -0,0 +1,25 @@
+package com.tianshouzhi.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Returns a 401 error code (Unauthorized) to the client, when Ajax authentication fails.
+ */
+@Component
+public class AjaxAuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        String localizedMessage = exception.getLocalizedMessage();
+        String message = exception.getMessage();
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Authentication failed");
+    }
+}
---
+++ form-login/form-login-ajax/src/main/webapp/auth/auth.js
@@ -0,0 +1,67 @@
+/**
+ * Created by tianshouzhi on 2017/12/19.
+ */
+angular.module('AngularApp')
+    .factory('Auth', function Auth($rootScope, $state, $q, $translate, Principal, AuthServerProvider) {
+        return {
+            login: function (credentials, callback) {
+                var cb = callback || angular.noop;
+                var deferred = $q.defer();
+
+                AuthServerProvider.login(credentials).then(function (data) {
+                    // retrieve the logged account information
+                    Principal.identity(true).then(function(account) {
+                        // After the login the language will be changed to
+                        // the language selected by the user during his registration
+                        $translate.use(account.langKey).then(function(){
+                            $translate.refresh();
+                        });
+                        deferred.resolve(data);
+                    });
+                    return cb();
+                }).catch(function (err) {
+                    this.logout();
+                    deferred.reject(err);
+                    return cb(err);
+                }.bind(this));
+
+                return deferred.promise;
+            },
+
+            logout: function () {
+                AuthServerProvider.logout();
+                Principal.authenticate(null);
+                // Reset state memory
+                $rootScope.previousStateName = undefined;
+                $rootScope.previousStateNameParams = undefined;
+            },
+
+            authorize: function(force) {
+                return Principal.identity(force)
+                    .then(function() {
+                        var isAuthenticated = Principal.isAuthenticated();
+
+                        // an authenticated user can't access to login and register pages
+                        if (isAuthenticated && $rootScope.toState.parent === 'account' && ($rootScope.toState.name === 'login' || $rootScope.toState.name === 'register')) {
+                            $state.go('home');
+                        }
+
+                        if ($rootScope.toState.data.authorities && $rootScope.toState.data.authorities.length > 0 && !Principal.hasAnyAuthority($rootScope.toState.data.authorities)) {
+                            if (isAuthenticated) {
+                                // user is signed in but not authorized for desired state
+                                $state.go('accessdenied');
+                            }
+                            else {
+                                // user is not authenticated. stow the state they wanted before you
+                                // send them to the signin state, so you can return them when you're done
+                                $rootScope.previousStateName = $rootScope.toState;
+                                $rootScope.previousStateNameParams = $rootScope.toStateParams;
+
+                                // now, send them to the signin state so they can log in
+                                $state.go('login');
+                            }
+                        }
+                    });
+            }
+        };
+    });
