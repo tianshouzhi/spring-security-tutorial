e13e9633de4e4fd52d24cd90baa199396d7053f9
tianshouzhi <tianshouzhi@126.com>
2017-12-19 00:00:22 +0800

    add form login

--- quickstart/quickstart-based-java/src/main/java/com/tianshouzhi/security/SecurityConfiguration.java
+++ quickstart/quickstart-based-java/src/main/java/com/tianshouzhi/security/SecurityConfiguration.java
@@ -1,18 +1,20 @@
 package com.tianshouzhi.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-/**
- * Created by tianshouzhi on 2017/12/16.
- */
 @EnableWebSecurity
 public class SecurityConfiguration {
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser( "user").password("password" ).roles("USER");
+    @Bean
+    public UserDetailsService userDetailsService() throws Exception {
+        UserDetails user = User
+                .withDefaultPasswordEncoder()
+                .username("tianshouzhi").password("tianshouzhi123").roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
