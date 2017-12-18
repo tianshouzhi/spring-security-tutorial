c0d9d7cea7d6f61622f2006d44858ee8e3e6f227
tianshouzhi <tianshouzhi@126.com>
2017-12-19 00:06:11 +0800

    optimize import

--- form-login/form-login-java/src/main/java/com/tianshouzhi/security/SecurityConfiguration.java
+++ form-login/form-login-java/src/main/java/com/tianshouzhi/security/SecurityConfiguration.java
@@ -1,11 +1,11 @@
 package com.tianshouzhi.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Created by tianshouzhi on 2017/12/18.
