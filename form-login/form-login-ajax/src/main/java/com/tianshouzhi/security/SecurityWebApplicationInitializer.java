024af5946354c43566b71b0813bf46c7b9a74537
tianshouzhi <tianshouzhi@126.com>
2017-12-19 23:34:20 +0800

    add form login angularjs

--- form-login/form-login-ajax/src/main/java/com/tianshouzhi/security/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.tianshouzhi.security;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-    public SecurityWebApplicationInitializer() {
-        super(SecurityConfiguration.class);
-    }
-}
